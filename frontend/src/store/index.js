3156fb1f7592666bfacca04d6b6421b0927acf0e
Jon Moore <jonevanmoore@yahoo.com>
2022-03-31 18:10:24 -0500

    csrfFetch working

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { restoreCSRF, csrfFetch } from './csrf';
 
 const rootReducer = combineReducers({
   // add reducer functions here
 });
+
 
 let enhancer;
 
@@ -19,5 +21,14 @@ if (process.env.NODE_ENV === "production") {
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
+
+const store = configureStore()
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 export default configureStore;
