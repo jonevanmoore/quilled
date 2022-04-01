6ec25d1f0e04b8ffeaba709b33d3d7af2f61a68c
Jon Moore <jonevanmoore@yahoo.com>
2022-04-01 12:11:19 -0500

    signup page working

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,12 +16,38 @@ const removeUser = () => {
     };
 };
 
+//LOGIN USER
 export const login = (user) => async (dispatch) => {
     const { credential, password } = user;
     const response = await csrfFetch('/api/session', {
         method: 'POST',
         body: JSON.stringify({
             credential,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+//KEEP USER LOGGED IN
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+
+//SIGN UP NEW USER
+export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            email,
             password,
         }),
     });
