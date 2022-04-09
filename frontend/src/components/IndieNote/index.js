dd855ba1b86f759654ab36cb6ae125838bd27ca1
Jon Moore <jonevanmoore@yahoo.com>
2022-04-09 11:07:12 -0500

    note can change notebooks now

--- frontend/src/components/IndieNote/index.js
+++ frontend/src/components/IndieNote/index.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Redirect, Link, useHistory, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import './IndieNote.css'
 
 import * as notesActions from '../../store/notes'
+import * as notebooksActions from '../../store/notebooks'
 
 export default function IndieNote() {
     const dispatch = useDispatch()
@@ -15,11 +17,18 @@ export default function IndieNote() {
 
     const note = useSelector(state => state.notes.notes[noteId])
 
+    const notebooks = useSelector(state => state.notebooks.notebooks)
+    const nbData = Object.values(notebooks)
+
     const [noteTitle, setNoteTitle] = useState(note?.title)
     const [noteContent, setNoteContent] = useState(note?.content)
+    const [notebookId, setNoteBookId] = useState(note?.notebookId || null)
+    const [readBtnColor, setReadBtnColor] = useState('color')
+    const [editBtnColor, setEditBtnColor] = useState('no-color')
 
     useEffect(() => {
         dispatch(notesActions.fetchNotes(userId))
+        dispatch(notebooksActions.fetchNotebooks(userId))
     }, [dispatch])
 
     const deleteNote = () => {
@@ -35,31 +44,52 @@ export default function IndieNote() {
             userId,
             id: noteId,
             title: noteTitle,
-            content: noteContent
+            content: noteContent,
+            notebookId: notebookId
         }))
     }
 
     return (
         <>
             <title>{`Quilled - ${noteTitle}`}</title>
-            <h2>Indie Note</h2>
-            <h2>{note?.title}</h2>
-            <h2>{note?.content}</h2>
-            <button onClick={deleteNote}>DELETE</button>
-            <label>EDIT</label>
-            <input
-                type='text'
-                value={noteTitle}
-                onChange={(e) => setNoteTitle(e?.target.value)}
-            >
-            </input>
-            <input
-                type='text'
-                value={noteContent}
-                onChange={(e) => setNoteContent(e?.target.value)}
-            >
-            </input>
-            <button onClick={editNote}>UPDATE</button>
+            <div className='indie-note-body'>
+                <div className='read-edit-btns'>
+                    <button className={`${readBtnColor} read-btn`}>Read</button>
+                    <button className={`${editBtnColor} edit-btn`}>Edit</button>
+                </div>
+
+                <h2>{note?.title}</h2>
+                <h2>{note?.content}</h2>
+                <button onClick={deleteNote}>DELETE</button>
+                <div className='edit-note-div'>
+
+                    <input
+                        type='text'
+                        value={noteTitle}
+                        onChange={(e) => setNoteTitle(e?.target.value)}
+                    >
+                    </input>
+                    <textarea
+                        type='text'
+                        value={noteContent}
+                        onChange={(e) => setNoteContent(e?.target.value)}
+                    >
+                    </textarea>
+                    <select
+                        name='notebookId'
+                        value={notebookId}
+                        onChange={(e) => setNoteBookId(e?.target.value)}
+                    >
+                        <option value={0}>None</option>
+                        {nbData.map(notebook => {
+                            return (
+                                <option key={notebook?.id} value={notebook?.id}>{notebook?.title}</option>
+                            )
+                        })}
+                    </select>
+                    <button onClick={editNote}>UPDATE</button>
+                </div>
+            </div>
         </>
     )
 }
